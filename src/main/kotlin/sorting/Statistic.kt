9bec7aeed3dafd9b5f1f14b85ed4da759c25b5fd
Igor <GI_Something@protonmail.com>
2020-10-09 17:46:38 +0300

    feat: Statistic shows longest/greatest element's occurrence percentage

--- src/main/kotlin/sorting/Statistic.kt
+++ src/main/kotlin/sorting/Statistic.kt
@@ -1,13 +1,16 @@
 package sorting
 
 import sorting.DataType.*
+import kotlin.math.roundToInt
 
 class Statistic(val max: String, val occurs: Int, val total: Int, val type: DataType) {
+    val occurrencePercentage = (occurs.toDouble() / total * 100).roundToInt()
+
     override fun toString(): String {
         return when (type) {
-            LONG -> "Total numbers: $total.\nThe greatest number: $max ($occurs time(s))."
-            WORD -> "Total words: $total.\nThe longest word: $max ($occurs times(s))."
-            LINE -> "Total lines: $total.\nThe longest line:\n$max\n($occurs times(s))."
-        }
+            LONG -> "Total numbers: $total.\nThe greatest number: $max "
+            WORD -> "Total words: $total.\nThe longest word: $max "
+            LINE -> "Total lines: $total.\nThe longest line:\n$max\n"
+        } + "($occurs time(s), $occurrencePercentage%)."
     }
 }
