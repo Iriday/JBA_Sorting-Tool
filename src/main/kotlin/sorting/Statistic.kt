4f1d3d79d6e5948747a86c28259f46b60803fc7b
Igor <GI_Something@protonmail.com>
2020-10-09 16:04:32 +0300

    feat: Impl data type "word"

--- src/main/kotlin/sorting/Statistic.kt
+++ src/main/kotlin/sorting/Statistic.kt
@@ -5,8 +5,9 @@ import sorting.DataType.*
 class Statistic(val max: String, val occurs: Int, val total: Int, val type: DataType) {
     override fun toString(): String {
         return when (type) {
-            LONG -> "Total numbers: ${total}.\nThe greatest number: $max ($occurs time(s))."
-            WORD, LINE -> throw NotImplementedError()
+            LONG -> "Total numbers: $total.\nThe greatest number: $max ($occurs time(s))."
+            WORD -> "Total words: $total.\nThe longest word: $max ($occurs times(s))."
+            LINE -> throw NotImplementedError()
         }
     }
 }
--- src/main/kotlin/sorting/main.kt
+++ src/main/kotlin/sorting/main.kt
@@ -8,17 +8,28 @@ fun main(args: Array<String>) {
 }
 
 fun run(parsedArgs: Args) {
+    val scn = Scanner(System.`in`)
     val statistic = when (parsedArgs.dataType) {
-        LONG -> processNumbers(readNumbersFromConsole(Scanner(System.`in`)))
-        WORD, LINE -> throw NotImplementedError()
+        LONG -> processNumbers(readNumbersFromConsole(scn))
+        WORD -> processWords(readWordsFromConsole(scn))
+        LINE -> throw NotImplementedError()
     }
     print(statistic)
 }
 
 fun readNumbersFromConsole(scn: Scanner) = scn.asSequence().map { it.toLong() }.toList()
 
+fun readWordsFromConsole(scn: Scanner) = scn.asSequence().toList()
+
 fun processNumbers(nums: List<Long>): Statistic {
-    val max = nums.maxOrNull() ?: throw IllegalArgumentException("The list is empty")
-    val occurs = nums.count { it == max }
-    return Statistic(max.toString(), occurs, nums.size, LONG)
+    val maxNumber = nums.maxOrNull() ?: throw IllegalArgumentException("The list is empty")
+    val occurs = nums.count { it == maxNumber }
+    return Statistic(maxNumber.toString(), occurs, nums.size, LONG)
+}
+
+fun processWords(words: List<String>): Statistic {
+    words.sorted()
+    val longestWord = words.maxByOrNull { it.length } ?: throw IllegalArgumentException("The list is empty")
+    val occurs = words.count { it == longestWord }
+    return Statistic(longestWord, occurs, words.size, WORD)
 }
